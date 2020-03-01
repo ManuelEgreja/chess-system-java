0b6bc7204dee9cf31181f8b6990b370368b36a3e
Manuel Egreja <egrejams@gmail.com>
2020-03-01 18:36:22 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -11,7 +11,7 @@ public class UI {
     			}
     		System.out.println();
     	}
-    	System.out.println("  a b c d e f g h");
+    	System.out.println("  a b c d e f g h");//dois espaços em branco parte baiso tabuleiro
      }
      
      private static void printPiece(ChessPiece piece) {
